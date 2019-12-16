2996e26b855b2617395637602faeb309f2fd3a1c
miniglock <davidbrin45@gmail.com>
2019-12-16 15:23:02 -0800

    Added button listener+starting functionality

--- js/main.js
+++ js/main.js
@@ -1,112 +1,446 @@
-// if i use jquery can use .toggleClass() to assign and unassign class in order to check turn. Use with .hasClass()
+// // if i use jquery can use .toggleClass() to assign and unassign class in order to check turn. Use with .hasClass()
 
-/*----- constants -----*/
-random name array for player names
+// /*----- constants -----*/
+// random name array for player names
+let deck = [
+  {
+    value: 2,
+    image: "card-deck/images/clubs/clubs-r02.svg"
+  },
+  {
+    value: 2,
+    image: "card-deck/images/diamonds/diamonds-r02.svg"
+  },
+  {
+    value: 2,
+    image: "card-deck/images/hearts/hearts-r02.svg"
+  },
+  {
+    value: 2,
+    image: "card-deck/images/spades/spades-r02.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/clubs/clubs-r03.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/diamonds/diamonds-r03.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/hearts/hearts-r03.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/spades/spades-r03.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/clubs/clubs-r04.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/diamonds/diamonds-r04.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/hearts/hearts-r04.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/spades/spades-r04.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/clubs/clubs-r05.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/diamonds/diamonds-r05.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/hearts/hearts-r05.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/spades/spades-r05.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/clubs/clubs-r06.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/diamonds/diamonds-r06.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/hearts/hearts-r06.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/spades/spades-r06.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/clubs/clubs-r07.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/diamonds/diamonds-r07.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/hearts/hearts-r07.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/spades/spades-r07.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/clubs/clubs-r08.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/diamonds/diamonds-r08.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/hearts/hearts-r08.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/spades/spades-r08.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/clubs/clubs-r09.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/diamonds/diamonds-r09.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/hearts/hearts-r09.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/spades/spades-r09.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-K.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-K.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-K.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-K.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/clubs/clubs-A.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/diamonds/diamonds-A.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/hearts/hearts-A.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/spades/spades-A.svg"
+  }
+];
+console.log(deck);
 
+// /*----- app's state (variables) -----*/
+// current bet p1, current bet p2, p1 cards, p2 cards, dealer cards, the deck, deal active
+let p1CB = 0;
+let p1w = 100;
+const player1Cards = {
+  value: [],
+  image: [],
+  turnComplete: false
+};
+const player2Cards = {
+  value: [],
+  image: [],
+  turnComplete: false
+};
+const dealerHand = {
+  value: [],
+  image: []
+};
 
-/*----- app's state (variables) -----*/
-current bet p1, current bet p2, p1 cards, p2 cards, dealer cards, the deck, deal active 
+// /*----- cached DOM element references -----*/
+let dealerCards = document.getElementById("dc");
+let dcard1 = document.getElementById("dcard1");
+let dcard2 = document.getElementById("dcard2");
+let p2Cards = document.getElementById("p2cards");
+let p2card1 = document.getElementById("p2card1");
+let p2card2 = document.getElementById("p2card2");
+let p2Wallet = document.getElementById("w2");
+let p2Bet = document.getElementById("cb2");
+let p1Cards = document.getElementById("p1cards");
+let p1card1 = document.getElementById("p1card1");
+let p1card2 = document.getElementById("p1card2");
+let p1Wallet = document.getElementById("w1");
+let p1Bet = document.getElementById("cb1");
+let dTotal = document.getElementById("dealer-tot");
+let stay1 = document.getElementById("stay1");
+let DD1 = document.getElementById("DD1");
+let hit1 = document.getElementById("hit1");
+let bet1 = document.getElementById("bet1");
+let ins1 = document.getElementById("ins1");
+let play1 = document.getElementById("play1");
+// /*----- event listeners -----*/
+// p1 buttons
+// document.getElementById("stay1").addEventListener("click");
+// document.getElementById("DD1").addEventListener("click");
+// document.getElementById("hit1").addEventListener("click");
+document.getElementById("bet1").addEventListener("click", betP1);
+// document.getElementById("ins1").addEventListener("click");
+document.getElementById("play1").addEventListener("click", player1Play);
+// // p2 buttons
+// document.getElementById("stay2").addEventListener("click");
+// document.getElementById("DD2").addEventListener("click");
+// document.getElementById("hit2").addEventListener("click");
+// document.getElementById("bet2").addEventListener("click");
+// document.getElementById("ins2").addEventListener("click");
+// document.getElementById("play2").addEventListener("click");
 
+// /*----- functions -----*/
+init();
+function init() {
+  bet1.disabled = false;
+  play1.disabled = true;
+  DD1.disabled = true;
+  ins1.disabled = true;
+  hit1.disabled = true;
+  stay1.disabled = true;
+}
+// init
+//     reset all state variables to default
+//     activate bet phase
+//     render
 
-/*----- cached DOM element references -----*/
-let dealerCards = document.getElementById("dc")
-let p2Cards = document.getElementById("p2cards")
-let p2Wallet = document.getElementById("w2")
-let p2Bet = document.getElementById("cb2")
-let p1Cards = document.getElementById("p1cards")
-let p2Wallet = document.getElementById("w1")
-let p2Bet = document.getElementById("cb1")
-/*----- event listeners -----*/
+function hitD() {
+  dealerHand.value.push(deck[0].value);
+  dealerHand.image.push(deck[0].image);
+  dcard1.src = dealerHand.image[dealerHand.image.length - 1];
+  dealerPlay();
+  deck.shift();
+  //   player1Cards.value.push(deck[0].value);
+  //   player1Cards.image.push(deck[0].image);
+  //   p1card1.src = player1Cards.image[player1Cards.image.length - 1];
+  //   deck.shift();
+}
+// hit
+//     add one card to player cards from deck
+//     if card total > 21 call stay call lose
+//     render
 
-/*----- functions -----*/
-run init
-
-init
-    reset all state variables to default
-    activate bet phase
-    render
-
-const shuffle = () =>{
-    let deckArray = deckArray.sort(() => Math.random() - 0.5)
-    deal()
-    render()
+function dealerPlay() {
+  dcard2.src = dealerHand.image[1];
+  let dealerTotal = dealerHand.value.reduce((a, b) => a + b, 0);
+  dTotal.innerText = dealerTotal;
+  if (dealerTotal < 17) {
+    hitD();
+    console.log("oh yeah");
+  } else {
+    console.log("oh no");
+    // stay();
+  }
 }
 
-deal 
-    deal 1 card down and 1 card up to players and dealer from deck
-    set all card states
-    if dealer card2 === ace call insurance
-    if dealer card total === 21 call lose
-    call if blackjack
-    render
-    
-bet
-    update current bet by 5 for appropriate player
-    check if current bet > current wallet
-    reduce player wallet by player bet amount
-    update play button state
-    render
+function deal() {
+  player1Cards.value.push(deck[0].value);
+  player1Cards.image.push(deck[0].image);
+  p1card1.src = player1Cards.image[0];
+  deck.shift();
+  player2Cards.value.push(deck[0].value);
+  player2Cards.image.push(deck[0].image);
+  p2card1.src = player2Cards.image[0];
+  deck.shift();
+  dealerHand.value.push(deck[0].value);
+  dealerHand.image.push(deck[0].image);
+  dcard1.src = dealerHand.image[0];
+  deck.shift();
+  player1Cards.value.push(deck[0].value);
+  player1Cards.image.push(deck[0].image);
+  p1card2.src = player1Cards.image[1];
+  deck.shift();
+  player2Cards.value.push(deck[0].value);
+  player2Cards.image.push(deck[0].image);
+  p2card2.src = player2Cards.image[1];
+  deck.shift();
+  dealerHand.value.push(deck[0].value);
+  dealerHand.image.push(deck[0].image);
+  deck.shift();
+  dealerPlay();
+}
 
-play
-    change deal active to yes
-    call shuffle
-    render
+function shuffle() {
+  deck = deck.sort(() => Math.random() - 0.5);
+  deal();
+}
+shuffle();
 
-insurance
-    pop insurance button
-    if dealer has blackjack
-    match bet amount and reduce wallet
-    call payout insurance
-    render
+//     deal 1 card down and 1 card up to players and dealer from deck
+//     set all card states
+//     if dealer card2 === ace call insurance
+//     if dealer card total === 21 call lose
+//     call if blackjack
+//     render
 
-double down
-    pop double down button
-    if yes double player current bet
-    reduce player wallet by player bet amount
-    call hit 
-    call stay 
-    render
+function betP1() {
+  p1CB += 5;
+  p1w -= 5;
+  p1Bet.innerText = p1CB;
+  p1Wallet.innerText = p1w;
+  if (p1CB > 0) {
+    play1.disabled = false;
+  }
+  if (p1w === 0) {
+    bet1.disabled = true;
+  }
+}
+// bet
+//     update current bet by 5 for appropriate player
+//     check if current bet > current wallet
+//     reduce player wallet by player bet amount
+//     update play button state
+//     render
 
-hit 
-    add one card to player cards from deck
-    if card total > 21 call stay call lose
-    render
+function player1Play() {
+  bet1.disabled = true;
+  play1.disabled = true;
+  DD1.disabled = false;
+  ins1.disabled = false;
+  hit1.disabled = false;
+  stay1.disabled = false;
+}
+// play
+//     change deal active to yes
+//     call shuffle
+//     render
 
-stay
-    if player 1 && 2 players player 2 turn 
-    if last active player call dealer play
-    render
+// insurance
+//     pop insurance button
+//     if dealer has blackjack
+//     match bet amount and reduce wallet
+//     call payout insurance
+//     render
 
-if blackjack
-    check if p1/p2 have blackjack
-    call payout blackjack
-    render
+// double down
+//     pop double down button
+//     if yes double player current bet
+//     reduce player wallet by player bet amount
+//     call hit
+//     call stay
+//     render
 
-payout blackjack
-    calculate appropriate amount
-    add amount to wallet
-    change game active to no
-    render
+// const stay = () => {
+//   bust();
+//   dealerPlay();
+// };
+//     if player 1 && 2 players player 2 turn
+//     call check for bust
+//     call dealer play
+//     render
 
-payout insurance
-    calculate appropriate amount
-    add amount to wallet
-    change game active to no
-    render
+// if blackjack
+//     check if p1/p2 have blackjack
+//     call payout blackjack
+//     render
 
-payout double down
-    calculate appropriate amount
-    add amount to wallet
-    change game active to no
-    render
+// check for bust
+//     if >21 call lose
+// payout blackjack
+//     calculate appropriate amount
+//     add amount to wallet
+//     change game active to no
+//     render
 
-tie 
-    if player card total === dealer card total || player && dealer bust increase wallet by current bet
-    render
+// payout insurance
+//     calculate appropriate amount
+//     add amount to wallet
+//     change game active to no
+//     render
 
-lose
+// payout double down
+//     calculate appropriate amount
+//     add amount to wallet
+//     change game active to no
+//     render
 
+// tie
+//     if player card total === dealer card total || player && dealer bust increase wallet by current bet
+//     render
 
-render 
-    take current state variables and update ui
+// lose
+//     alert a message you lost
 
+// render
+//     take current state variables and update ui
