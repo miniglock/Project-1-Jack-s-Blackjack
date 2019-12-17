3a1ea26ede56d137d1e8cd4465490e3938e24bb5
miniglock <davidbrin45@gmail.com>
2019-12-17 10:53:33 -0800

    Added functionality

--- js/main.js
+++ js/main.js
@@ -212,7 +212,7 @@ let deck = [
     image: "card-deck/images/spades/spades-A.svg"
   }
 ];
-let deck1 = [
+const deck1 = [
   {
     value: 2,
     image: "card-deck/images/clubs/clubs-r02.svg"
@@ -444,15 +444,17 @@ const dealerHand = {
   value: [],
   image: []
 };
-let p1Total = player1Cards.value.reduce((a, b) => a + b, 0);
+let p1Total = 0;
+let p2Total = 0;
 
 // /*----- cached DOM element references -----*/
 let dealerCards = document.getElementById("dc");
 let dcard1 = document.getElementById("dcard1");
 let dcard2 = document.getElementById("dcard2");
+let dTotal = document.getElementById("dealer-tot");
 let p2Cards = document.getElementById("p2cards");
-let p2card1 = document.getElementById("p2card1");
-let p2card2 = document.getElementById("p2card2");
+let p2Card1 = document.getElementById("p2card1");
+let p2Card2 = document.getElementById("p2card2");
 let p2Wallet = document.getElementById("w2");
 let p2Bet = document.getElementById("cb2");
 let p1Cards = document.getElementById("p1cards");
@@ -460,18 +462,23 @@ let p1Card1 = document.getElementById("p1card1");
 let p1card2 = document.getElementById("p1card2");
 let p1Wallet = document.getElementById("w1");
 let p1Bet = document.getElementById("cb1");
-let dTotal = document.getElementById("dealer-tot");
 let stay1 = document.getElementById("stay1");
 let DD1 = document.getElementById("DD1");
 let hit1 = document.getElementById("hit1");
 let bet1 = document.getElementById("bet1");
 let ins1 = document.getElementById("ins1");
 let play1 = document.getElementById("play1");
+let stay2 = document.getElementById("stay2");
+let DD2 = document.getElementById("DD2");
+let hit2 = document.getElementById("hit2");
+let bet2 = document.getElementById("bet2");
+let ins2 = document.getElementById("ins2");
+let play2 = document.getElementById("play2");
 let player1Total = document.getElementById("p1total");
 let player2Total = document.getElementById("p2total");
 // /*----- event listeners -----*/
 // p1 buttons
-// document.getElementById("stay1").addEventListener("click");
+document.getElementById("stay1").addEventListener("click", player1Stay);
 // document.getElementById("DD1").addEventListener("click");
 document.getElementById("hit1").addEventListener("click", hitP1);
 document.getElementById("bet1").addEventListener("click", betP1);
@@ -480,10 +487,10 @@ document.getElementById("play1").addEventListener("click", player1Play);
 // // p2 buttons
 // document.getElementById("stay2").addEventListener("click");
 // document.getElementById("DD2").addEventListener("click");
-// document.getElementById("hit2").addEventListener("click");
-// document.getElementById("bet2").addEventListener("click");
+document.getElementById("hit2").addEventListener("click", hitP2);
+document.getElementById("bet2").addEventListener("click", betP2);
 // document.getElementById("ins2").addEventListener("click");
-// document.getElementById("play2").addEventListener("click");
+document.getElementById("play2").addEventListener("click", player2Play);
 
 // /*----- functions -----*/
 init();
@@ -495,16 +502,18 @@ function init() {
   ins1.disabled = true;
   hit1.disabled = true;
   stay1.disabled = true;
-  bet2.disabled = true;
+  bet2.disabled = false;
   play2.disabled = true;
   DD2.disabled = true;
   ins2.disabled = true;
   hit2.disabled = true;
   stay2.disabled = true;
-  p1CB = 0;
-  p2CB = 0;
-  p1w = 100;
-  p2w = 100;
+  p1Bet.innerText = 0;
+  p1Wallet.innerText = 100;
+  p2Bet.innerText = 0;
+  p2Wallet.innerText = 100;
+  p1Total = 0;
+  p2Total = 0;
 }
 // init
 //     reset all state variables to default
@@ -529,6 +538,13 @@ function hitP1() {
   deck.shift();
   render();
 }
+function hitP2() {
+  player2Cards.value.push(deck[0].value);
+  player2Cards.image.push(deck[0].image);
+  p2Card1.src = player2Cards.image[player2Cards.image.length - 1];
+  deck.shift();
+  render();
+}
 // hit
 //     add one card to player cards from deck
 //     if card total > 21 call stay call lose
@@ -542,7 +558,7 @@ function dealerPlay() {
     console.log("oh yeah");
   } else {
     console.log("oh no");
-    // stay();
+    // dealerStay();
   }
   render();
 }
@@ -552,10 +568,12 @@ function deal() {
   player1Cards.image.push(deck[0].image);
   p1Card1.src = player1Cards.image[0];
   deck.shift();
-  player2Cards.value.push(deck[0].value);
-  player2Cards.image.push(deck[0].image);
-  p2card1.src = player2Cards.image[0];
-  deck.shift();
+  if (p2CB > 0) {
+    player2Cards.value.push(deck[0].value);
+    player2Cards.image.push(deck[0].image);
+    p2card1.src = player2Cards.image[0];
+    deck.shift();
+  }
   dealerHand.value.push(deck[0].value);
   dealerHand.image.push(deck[0].image);
   dcard1.src = dealerHand.image[0];
@@ -564,10 +582,12 @@ function deal() {
   player1Cards.image.push(deck[0].image);
   p1card2.src = player1Cards.image[1];
   deck.shift();
-  player2Cards.value.push(deck[0].value);
-  player2Cards.image.push(deck[0].image);
-  p2card2.src = player2Cards.image[1];
-  deck.shift();
+  if (p2CB > 0) {
+    player2Cards.value.push(deck[0].value);
+    player2Cards.image.push(deck[0].image);
+    p2card2.src = player2Cards.image[1];
+    deck.shift();
+  }
   dealerHand.value.push(deck[0].value);
   dealerHand.image.push(deck[0].image);
   deck.shift();
@@ -590,6 +610,18 @@ function betP1() {
     bet1.disabled = true;
   }
 }
+function betP2() {
+  p2CB += 5;
+  p2w -= 5;
+  p2Bet.innerText = p2CB;
+  p2Wallet.innerText = p2w;
+  //   if (p2CB > 0) {
+  //     play2.disabled = false;
+  //   }
+  if (p2w === 0) {
+    bet2.disabled = true;
+  }
+}
 // bet
 //     update current bet by 5 for appropriate player
 //     check if current bet > current wallet
@@ -606,6 +638,17 @@ function player1Play() {
   stay1.disabled = false;
   shuffle();
   render();
+}
+function player2Play() {
+  if (p1Total > 0) {
+    bet2.disabled = true;
+    play2.disabled = true;
+    DD2.disabled = false;
+    ins2.disabled = false;
+    hit2.disabled = false;
+    stay2.disabled = false;
+    render();
+  }
 }
 // play
 //     change deal active to yes
@@ -630,10 +673,19 @@ function insurance() {
 //     call stay
 //     render
 
-// const stay = () => {
-//   bust();
-//   dealerPlay();
-// };
+function player1Stay() {
+  if (p2CB > 0) {
+    play2.disabled = false;
+  }
+  bet1.disabled = true;
+  play1.disabled = true;
+  DD1.disabled = true;
+  ins1.disabled = false;
+  hit1.disabled = true;
+  stay1.disabled = true;
+  bust();
+  dealerPlay();
+}
 //     if player 1 && 2 players player 2 turn
 //     call check for bust
 //     call dealer play
@@ -672,9 +724,17 @@ function p1Lose() {
   bet1.disabled = true;
   play1.disabled = true;
   DD1.disabled = true;
-  ins1.disabled = true;
+  //   ins1.disabled = true;
   hit1.disabled = true;
   stay1.disabled = true;
+}
+function p2Lose() {
+  bet2.disabled = true;
+  play2.disabled = true;
+  DD2.disabled = true;
+  ins2.disabled = true;
+  hit2.disabled = true;
+  stay2.disabled = true;
 }
 // lose
 //     alert a message you lost
@@ -682,12 +742,23 @@ function p1Lose() {
 function render() {
   p1Total = player1Cards.value.reduce((a, b) => a + b, 0);
   player1Total.innerText = p1Total;
+  p2Total = player2Cards.value.reduce((a, b) => a + b, 0);
+  player2Total.innerText = p2Total;
   dTotal.innerText = dealerTotal;
   if (p1Total > 21) {
     console.log("bust");
-    // stay();
+    player1Stay();
     p1Lose();
+  } else if (p1Total === 21) {
+    hit1.disabled = true;
+  }
+  if (p2Total > 21) {
+    console.log("bust");
+    // player2Stay();
+    p2Lose();
+  } else if (p2Total === 21) {
+    hit2.disabled = true;
   }
 }
 // render
-//     take current state variables and update ui
+//     take current state variables and update
