32b4dda1b2e559629c1912501bc1c2e2d8b15874
miniglock <davidbrin45@gmail.com>
2019-12-16 18:04:38 -0800

    Add ptotals some functionality

--- js/main.js
+++ js/main.js
@@ -212,12 +212,224 @@ let deck = [
     image: "card-deck/images/spades/spades-A.svg"
   }
 ];
-console.log(deck);
+let deck1 = [
+  {
+    value: 2,
+    image: "card-deck/images/clubs/clubs-r02.svg"
+  },
+  {
+    value: 2,
+    image: "card-deck/images/diamonds/diamonds-r02.svg"
+  },
+  {
+    value: 2,
+    image: "card-deck/images/hearts/hearts-r02.svg"
+  },
+  {
+    value: 2,
+    image: "card-deck/images/spades/spades-r02.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/clubs/clubs-r03.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/diamonds/diamonds-r03.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/hearts/hearts-r03.svg"
+  },
+  {
+    value: 3,
+    image: "card-deck/images/spades/spades-r03.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/clubs/clubs-r04.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/diamonds/diamonds-r04.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/hearts/hearts-r04.svg"
+  },
+  {
+    value: 4,
+    image: "card-deck/images/spades/spades-r04.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/clubs/clubs-r05.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/diamonds/diamonds-r05.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/hearts/hearts-r05.svg"
+  },
+  {
+    value: 5,
+    image: "card-deck/images/spades/spades-r05.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/clubs/clubs-r06.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/diamonds/diamonds-r06.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/hearts/hearts-r06.svg"
+  },
+  {
+    value: 6,
+    image: "card-deck/images/spades/spades-r06.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/clubs/clubs-r07.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/diamonds/diamonds-r07.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/hearts/hearts-r07.svg"
+  },
+  {
+    value: 7,
+    image: "card-deck/images/spades/spades-r07.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/clubs/clubs-r08.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/diamonds/diamonds-r08.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/hearts/hearts-r08.svg"
+  },
+  {
+    value: 8,
+    image: "card-deck/images/spades/spades-r08.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/clubs/clubs-r09.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/diamonds/diamonds-r09.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/hearts/hearts-r09.svg"
+  },
+  {
+    value: 9,
+    image: "card-deck/images/spades/spades-r09.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-r10.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-J.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-Q.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/clubs/clubs-K.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/diamonds/diamonds-K.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/hearts/hearts-K.svg"
+  },
+  {
+    value: 10,
+    image: "card-deck/images/spades/spades-K.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/clubs/clubs-A.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/diamonds/diamonds-A.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/hearts/hearts-A.svg"
+  },
+  {
+    value: 11,
+    image: "card-deck/images/spades/spades-A.svg"
+  }
+];
 
 // /*----- app's state (variables) -----*/
 // current bet p1, current bet p2, p1 cards, p2 cards, dealer cards, the deck, deal active
 let p1CB = 0;
+let p2CB = 0;
 let p1w = 100;
+let p2w = 100;
+let dealerTotal = null;
 const player1Cards = {
   value: [],
   image: [],
@@ -232,6 +444,7 @@ const dealerHand = {
   value: [],
   image: []
 };
+let p1Total = player1Cards.value.reduce((a, b) => a + b, 0);
 
 // /*----- cached DOM element references -----*/
 let dealerCards = document.getElementById("dc");
@@ -243,7 +456,7 @@ let p2card2 = document.getElementById("p2card2");
 let p2Wallet = document.getElementById("w2");
 let p2Bet = document.getElementById("cb2");
 let p1Cards = document.getElementById("p1cards");
-let p1card1 = document.getElementById("p1card1");
+let p1Card1 = document.getElementById("p1card1");
 let p1card2 = document.getElementById("p1card2");
 let p1Wallet = document.getElementById("w1");
 let p1Bet = document.getElementById("cb1");
@@ -254,13 +467,15 @@ let hit1 = document.getElementById("hit1");
 let bet1 = document.getElementById("bet1");
 let ins1 = document.getElementById("ins1");
 let play1 = document.getElementById("play1");
+let player1Total = document.getElementById("p1total");
+let player2Total = document.getElementById("p2total");
 // /*----- event listeners -----*/
 // p1 buttons
 // document.getElementById("stay1").addEventListener("click");
 // document.getElementById("DD1").addEventListener("click");
-// document.getElementById("hit1").addEventListener("click");
+document.getElementById("hit1").addEventListener("click", hitP1);
 document.getElementById("bet1").addEventListener("click", betP1);
-// document.getElementById("ins1").addEventListener("click");
+document.getElementById("ins1").addEventListener("click", insurance);
 document.getElementById("play1").addEventListener("click", player1Play);
 // // p2 buttons
 // document.getElementById("stay2").addEventListener("click");
@@ -273,12 +488,23 @@ document.getElementById("play1").addEventListener("click", player1Play);
 // /*----- functions -----*/
 init();
 function init() {
+  deck = deck1;
   bet1.disabled = false;
   play1.disabled = true;
   DD1.disabled = true;
   ins1.disabled = true;
   hit1.disabled = true;
   stay1.disabled = true;
+  bet2.disabled = true;
+  play2.disabled = true;
+  DD2.disabled = true;
+  ins2.disabled = true;
+  hit2.disabled = true;
+  stay2.disabled = true;
+  p1CB = 0;
+  p2CB = 0;
+  p1w = 100;
+  p2w = 100;
 }
 // init
 //     reset all state variables to default
@@ -293,8 +519,15 @@ function hitD() {
   deck.shift();
   //   player1Cards.value.push(deck[0].value);
   //   player1Cards.image.push(deck[0].image);
-  //   p1card1.src = player1Cards.image[player1Cards.image.length - 1];
+  //   p1Card1.src = player1Cards.image[player1Cards.image.length - 1];
   //   deck.shift();
+}
+function hitP1() {
+  player1Cards.value.push(deck[0].value);
+  player1Cards.image.push(deck[0].image);
+  p1Card1.src = player1Cards.image[player1Cards.image.length - 1];
+  deck.shift();
+  render();
 }
 // hit
 //     add one card to player cards from deck
@@ -303,8 +536,7 @@ function hitD() {
 
 function dealerPlay() {
   dcard2.src = dealerHand.image[1];
-  let dealerTotal = dealerHand.value.reduce((a, b) => a + b, 0);
-  dTotal.innerText = dealerTotal;
+  dealerTotal = dealerHand.value.reduce((a, b) => a + b, 0);
   if (dealerTotal < 17) {
     hitD();
     console.log("oh yeah");
@@ -312,12 +544,13 @@ function dealerPlay() {
     console.log("oh no");
     // stay();
   }
+  render();
 }
 
 function deal() {
   player1Cards.value.push(deck[0].value);
   player1Cards.image.push(deck[0].image);
-  p1card1.src = player1Cards.image[0];
+  p1Card1.src = player1Cards.image[0];
   deck.shift();
   player2Cards.value.push(deck[0].value);
   player2Cards.image.push(deck[0].image);
@@ -338,21 +571,12 @@ function deal() {
   dealerHand.value.push(deck[0].value);
   dealerHand.image.push(deck[0].image);
   deck.shift();
-  dealerPlay();
 }
 
 function shuffle() {
   deck = deck.sort(() => Math.random() - 0.5);
   deal();
 }
-shuffle();
-
-//     deal 1 card down and 1 card up to players and dealer from deck
-//     set all card states
-//     if dealer card2 === ace call insurance
-//     if dealer card total === 21 call lose
-//     call if blackjack
-//     render
 
 function betP1() {
   p1CB += 5;
@@ -380,12 +604,17 @@ function player1Play() {
   ins1.disabled = false;
   hit1.disabled = false;
   stay1.disabled = false;
+  shuffle();
+  render();
 }
 // play
 //     change deal active to yes
 //     call shuffle
 //     render
 
+function insurance() {
+  dealerPlay();
+}
 // insurance
 //     pop insurance button
 //     if dealer has blackjack
@@ -439,8 +668,26 @@ function player1Play() {
 //     if player card total === dealer card total || player && dealer bust increase wallet by current bet
 //     render
 
+function p1Lose() {
+  bet1.disabled = true;
+  play1.disabled = true;
+  DD1.disabled = true;
+  ins1.disabled = true;
+  hit1.disabled = true;
+  stay1.disabled = true;
+}
 // lose
 //     alert a message you lost
 
+function render() {
+  p1Total = player1Cards.value.reduce((a, b) => a + b, 0);
+  player1Total.innerText = p1Total;
+  dTotal.innerText = dealerTotal;
+  if (p1Total > 21) {
+    console.log("bust");
+    // stay();
+    p1Lose();
+  }
+}
 // render
 //     take current state variables and update ui
